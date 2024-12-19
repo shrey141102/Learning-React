bd9b7b5ab0a6cca308f015e697a3171669a9c18c
Shreyansh Khaitan <shrey14112002@gmail.com>
2024-12-19 14:56:51 +0530

    initial

---
+++ learning-react/src/Food.jsx
@@ -0,0 +1,17 @@
+
+
+function Food(){
+
+    const food1 = "Orange";
+    const food2 = "Apple";
+
+    return(
+        <ul>
+            <li>Watermelon</li>
+            <li>{food1}</li>
+            <li>{food2}</li>
+        </ul>
+    )
+}
+
+export default Food;
